68eba52ffc536e16d7e6d821783dcd5b924719f3
Zunayed Ali <zunayed@gmail.com>
2014-09-30 18:43:20 -0400

    allowing stat selector

--- static/maps.js
+++ static/maps.js
@@ -1,23 +1,19 @@
 /*global d3: false  */
 "use strict";
 
+//set up map and default dataset
+var w = screen.width;
+var h = screen.height;
 var curr_team = "New York Knicks";
 var curr_team_abbr = "NYK"
 var curr_stat = "FT_PCT"
-
-//data
 var nba_data;
-var stadium_data;
 var states_data;
-
-//reading data file first to get values
-d3.json("static/data/league_data_averaged.json", function(data){
-    nba_data = data[curr_team];
-});
-
-//set up map
-var w = screen.width;
-var h = screen.height;
+var setColor;   // Quantize function
+var setSize;    // Quantize function
+var svg;
+var counties
+var point_group;
 
 //map type & center point 
 var projection = d3.geo.mercator()
@@ -26,43 +22,96 @@ var projection = d3.geo.mercator()
 
 var path = d3.geo.path().projection(projection);
 
-//set up a quantize function to associate with colorbrew styles
-var setColor = d3.scale.quantize()
-    .domain([.605, .902])
-    .range(colorbrewer.Greens[5]);
-
-//set up function for radius size
-var setSize = d3.scale.quantize()
-    .domain([.605, .902])
-    .range(d3.range(4,20).map(function(i) { return i; }));
-
-//initialize map
-var svg = d3.select("#d3_map")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
-
-// create a container for counties
-var counties = svg.append("g")
-    .attr("id", "counties");
-
-// create container for stadium points
-var point_group = svg.append("g");
+//reading data file first to get values
+d3.json("static/data/league_data_averaged.json", function(data){
+    nba_data = data[curr_team];
+});
 
 //reading geoJSON & CSV files
 d3.json("static/data/us_states_small_size.json", function(states){
     states_data = states
+    initializeSVG()
     createMap(states_data);
+    plotData();
 });
 
-//reading data file
-d3.csv("static/data/stadium_geo_data.csv", function(data){
-    stadium_data = data
-    createPoints(stadium_data);
-});
+var initializeSVG = function(domain){
+    //initialize map
+    svg = d3.select("#d3_map")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
 
-var calc_min_max = function(data) {
-    var min_value = 1;
+    // create a container for counties & stadium points
+    counties = svg.append("g")
+        .attr("id", "counties");
+
+    point_group = svg.append("g");
+};
+
+
+
+var setQuantizeFunc = function(domain){
+    console.log(domain)
+    //set up a quantize function to associate with colorbrew styles
+    setColor = d3.scale.quantize()
+        .domain(domain)
+        .range(colorbrewer.Greens[8].slice(1));
+
+    //set up function for radius size
+    setSize = d3.scale.quantize()
+        .domain(domain)
+        .range(d3.range(6,35).map(function(i) { return i; }));
+};
+
+//draw world map
+var createMap = function(states) {
+    counties.append("g")
+        .selectAll("path")
+        .data(states.features)
+        .enter()
+        .append("path")
+        .style("fill", "#A9BCF5")
+        .attr("stroke", "#fff")
+        .attr("d", path);
+};
+
+var plotData = function() {
+    //reading data file
+    setQuantizeFunc(calcMinMax(nba_data));
+
+    d3.csv("static/data/stadium_geo_data.csv", function(data){
+        point_group.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", function(d){ return projection([d.lon, d.lat])[0]; })
+            .attr("cy", function(d){ return projection([d.lon, d.lat])[1]; })
+            .attr("shortname", function(d){ return d.shortname; })
+            .attr("team", function(d){ return d.team; })
+            .attr("r", function(d){ return radSize(d.shortname); })
+            .attr("fill", function(d){ return radColor(d.shortname); })
+            .on("mouseover", mouseOver)
+            .on("mouseout", mouseOut)
+    });
+};
+
+var mouseOver = function() {
+    d3.select(this).style("stroke", "green");
+    d3.select(this).style("stroke-width", "4px");
+    var team_name = d3.select(this).attr("team");
+    var team_abbr = d3.select(this).attr("shortname");
+    var stat_value = Math.round(nba_data[team_abbr][curr_stat] * 100)/100;
+    d3.select("#infoBox").html( curr_stat + " - " + team_name + " : " + stat_value);
+};
+
+var mouseOut = function() {
+    d3.select(this).style("stroke", "");
+    d3.select(this).style("stroke-width", "");
+};
+
+var calcMinMax = function(data) {
+    var min_value = 200;
     var max_value = 0;
 
     for (var team in data) {
@@ -76,12 +125,13 @@ var calc_min_max = function(data) {
     return [min_value, max_value];
 };
 
-var radSize = function(place) {
-    if(place == curr_team_abbr) {
+// determine size of circle
+var radSize = function(shortname) {
+    if(shortname == curr_team_abbr) {
         return 0
     }else {
         try {
-            return setSize(nba_data[place][curr_stat]);
+            return setSize(nba_data[shortname][curr_stat]);
         }
         catch (e) {
             console.log(e);
@@ -90,71 +140,26 @@ var radSize = function(place) {
     }
 };
 
-var radColor = function(place) {
-    if(place == curr_team_abbr) {
+// determine color of circle
+var radColor = function(shortname) {
+    if(shortname == curr_team_abbr) {
         return setColor(0);
     }else {
         try {
-            return setColor(nba_data[place][curr_stat]);
+            return setColor(nba_data[shortname][curr_stat]);
         }
         catch (e) {
-            console.log(e); // pass exception object to error handler
+            console.log(e); 
             return setColor(0);
         }
     }
 };
 
-//draw world map
-var createMap = function (states) {
-    counties.append("g")
-        .selectAll("path")
-        .data(states.features)
-        .enter()
-        .append("path")
-        .style("fill", "#43a2ca")
-        // .attr("state", function(d) { return d.properties['NAME'] } )
-        // .attr("class", function(d) { return stateColor(d.properties['NAME']) } )
-        .attr("stroke", "#fff")
-        .attr("d", path);
-};
-
-
-// place dots on screen based on cities
-var createPoints = function(json){
-    point_group.selectAll("circle")
-        .data(json)
-        .enter()
-        .append("circle")
-        .attr("cx", function(d){ return projection([d.lon, d.lat])[0]; })
-        .attr("cy", function(d){ return projection([d.lon, d.lat])[1]; })
-        .attr("place", function(d){ return d.place; })
-        .attr("team", function(d){ return d.team; })
-        .attr("r", function(d){ return radSize(d.place)})
-        // .style("fill", "red")
-        .attr("fill", function(d){ return radColor(d.place) } )
-
-        .on("mouseover", mouseover)
-        .on("mouseout", mouseout)
-};
-
-var mouseover = function() {
-    d3.select(this).style("stroke", "green");
-    d3.select(this).style("stroke-width", "4px");
-    var team_name = d3.select(this).attr("team");
-    d3.select("#infoBox").html("Team: " + team_name);
-};
-
-var mouseout = function() {
-    d3.select(this).style("stroke", "");
-    d3.select(this).style("stroke-width", "");
-};
-
 //monitor dropdown menu to change map data
-// d3.select("#dataSelector").on("change", function() {
-//     current = dataSets[this.value]
-
-//     setColor.domain([current.minDomain, current.maxDomain]);
-//     createMap(states_data);
-
-//     counties.attr("class", current.color);
-// });
+d3.select("#dataSelector").on("change", function() {
+    curr_stat = this.value;
+    console.log(curr_stat);
+    point_group.remove();
+    point_group = svg.append("g");
+    plotData()
+});
